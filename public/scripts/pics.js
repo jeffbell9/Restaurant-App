751a74339f150075d95f5c233b10b572ee68179a
Jeff Bell <jeffbell@Jeffs-MacBook-Pro.local>
2017-04-23 21:56:07 -0700

    Deleted links and made single page

--- public/scripts/pics.js
+++ public/scripts/pics.js
@@ -1,5 +1,6 @@
 $(document).ready(function() {
 	$("#scroll").html('<img src="img/ornament-scroll-rule-300px.png" alt="scroll">');
+	$("#scroll-two").html('<img src="img/ornament-scroll-rule-300px.png" alt="scroll">');
 	$("#tower-left").html('<img class="img-responsive" src="img/eiffel-tower-charcoal-sketch-800px.png" alt="Eiffel Tower">');
 	$("#tower-right").html('<img class="img-responsive" src="img/eiffel-tower-charcoal-sketch-800px.png" alt="Eiffel Tower">');
 	$("#tower-left-wine").html('<img class="img-responsive" src="img/eiffel-tower-charcoal-sketch-800px.png" alt="Eiffel Tower">');
